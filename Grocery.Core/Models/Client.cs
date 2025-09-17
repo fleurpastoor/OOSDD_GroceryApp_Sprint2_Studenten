dd0c1a4567c2ee1ca1a4931cd6f1204579bb788a
fleurpastoor <s1212464@student.windesheim.nl>
2025-09-17 20:22:50 +0200

    Followed steps and completed the login functionality (uc6)

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -3,8 +3,8 @@ namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
-        private string _emailAddress;
-        private string _password { get; set; }
+        public string _emailAddress { get; set; }
+        public string _password { get; set; }
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
             _emailAddress=emailAddress;
